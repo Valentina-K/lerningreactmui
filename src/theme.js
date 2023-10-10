dbf397901bc88b6efa78b2d868d156dc6a12c476
Valentшna-K <kvalentyna@hotmail.com>
2023-10-10 15:49:28 +0300

    ok

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#005200",
+    },
+    secondary: {
+      main: "#edf2ff",
+    },
+  },
+});
